5a63b228594e39121dfbcc5265c4646ef5950e0e
Anton <san0173@mail.ru>
2024-02-20 21:45:53 +0400

    Add project files.

---
+++ Demo.cs
@@ -0,0 +1,44 @@
+﻿namespace CQGTesting;
+
+/// <summary>
+/// Демонстрация программы.
+/// </summary>
+public static class Demo
+{
+    public static void StartProgram(string[] args)
+    {
+        if (args.Length != 2)
+        {
+            Console.WriteLine("\nНеверное число аргументов!");
+            return;
+        }
+
+        var configFile = args[0];
+        var sampleFile = args[1];
+
+        try
+        {
+            var sortedSampleDictionary = OperationTextFile.SortedSampleTextChange(configFile, sampleFile);
+
+            Console.WriteLine("\nОтсортированный по убыванию по измененниям текст и количество изменений в нём\n");
+
+            foreach (var item in sortedSampleDictionary)
+            {
+                Console.WriteLine($"{item.Key}  :  {item.Value}");
+            }
+        }
+        catch (Exception)
+        {
+            if (!File.Exists(configFile))
+            {
+                Console.WriteLine("Не найден введённый конфигурационный файл, проверьте правильность введенного названия!");
+            }
+
+            if (!File.Exists(sampleFile))
+            {
+                Console.WriteLine("Не найден введённый файл с примером текста, проверьте правильность введенного названия!");
+            }
+
+        }
+    }
+}
---
+++ OperationTextFile.cs
@@ -0,0 +1,111 @@
+﻿namespace CQGTesting;
+
+/// <summary>
+/// Операции над текстовыми файлами 
+/// </summary>
+public class OperationTextFile
+{
+    /// <summary>
+    /// Запись в словарь данных из конфигуриционного файла.
+    /// </summary>
+    /// <param name="path">Имя файла.</param>
+    /// <returns>Словарь с данными.</returns>
+    private static Dictionary<string, string> ReadConfigurationFile(string path)
+    {
+        var configDictionary = new Dictionary<string, string>();
+
+        using var streamReader = new StreamReader(path);
+
+        while (!streamReader.EndOfStream)
+        {
+            var configLine = streamReader.ReadLine();
+
+            if (configLine is not null)
+            {
+                var configPair = configLine.Split('=');
+
+                configDictionary.Add(configPair[0], configPair[1]);
+            }
+
+        }
+
+        return configDictionary;
+    }
+
+    /// <summary>
+    /// Запись в словарь данных из файла с текстом.
+    /// </summary>
+    /// <param name="path">Имя файла</param>
+    /// <returns>Словарь с текстом и нулевым количеством изменений в нём.</returns>
+    private static Dictionary<string, int> ReadSampleTextFile(string path)
+    {
+        var textDictionary = new Dictionary<string, int>();
+        var countChanged = 0;
+
+        using var streamReader = new StreamReader(path);
+
+        while (!streamReader.EndOfStream)
+        {
+            var sampleLine = streamReader.ReadLine();
+
+            if (sampleLine is not null)
+            {
+                textDictionary.Add(sampleLine, countChanged);
+            }
+        }
+
+        return textDictionary;
+    }
+
+    /// <summary>
+    /// Изменение текста из примерочного файла и количество изменений в нём.
+    /// </summary>
+    /// <param name="pathConfig">Имя конфигурационного файла.</param>
+    /// <param name="pathSample">Имя файла с текстом.</param>
+    /// <returns>Словарь с изменённым текстом и количеством изменений в нём.</returns>
+    private static Dictionary<string, int> ChangeValueText(string pathConfig, string pathSample)
+    {
+        var configDictionary = ReadConfigurationFile(pathConfig);
+        var sampleDictionary = ReadSampleTextFile(pathSample);
+        var changedSampleText = new Dictionary<string, int>();
+        var countChange = 0;
+
+        foreach (var itemSample in sampleDictionary)
+        {
+            var sampleText = itemSample.Key;
+
+            foreach (var itemConfig in configDictionary)
+            {
+                sampleText = sampleText.Replace(itemConfig.Key, itemConfig.Value);
+
+                countChange = itemSample.Key
+                                        .Zip(sampleText, (charSampleText, charChangedSampleText) =>
+                                             charSampleText == charChangedSampleText ? 0 : 1)
+                                        .Sum();
+            }
+
+            changedSampleText.Add(sampleText, countChange);
+        }
+
+        return changedSampleText;
+    }
+
+    /// <summary>
+    /// Сортировка текста по количеству изменений в нём.
+    /// </summary>
+    /// <param name="pathConfig">Имя конфигурационного файла</param>
+    /// <param name="pathSample">Имя файла с текстом.</param>
+    /// <returns>Словарь с отсортированным текстом и количеством изменений по убыванию.</returns>
+    public static Dictionary<string, int> SortedSampleTextChange(string pathConfig, string pathSample)
+    {
+        var changedSampleText = ChangeValueText(pathConfig, pathSample);
+
+        if (string.IsNullOrWhiteSpace(pathSample) || string.IsNullOrWhiteSpace(pathConfig))
+        {
+            return changedSampleText;
+        }
+
+        return changedSampleText.OrderByDescending(x => x.Value)
+                                .ToDictionary(x => x.Key, x => x.Value);
+    }
+}
